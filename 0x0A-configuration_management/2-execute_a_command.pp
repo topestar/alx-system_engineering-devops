bcf9eb53fe30b7763b5959bcb9451ea0a6c34b27
topestar <topestar@live.com>
2023-03-24 15:35:02 -0700

    task added

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,5 @@
-#kills killmeow
-exec {'pkill killmenow':
+# execute pkill command to kill bashscript file killmenow
+exec { 'pkill':
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
